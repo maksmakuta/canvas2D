29fe6efb9c894812ecaaace3b6b3b0bd2b4e0985
Maks Makuta <makuta.maks.mmc@gmail.com>
2025-04-13 20:52:10 +0300

    [0.2.0] implement Path API

--- include/canvas2D/Path.hpp
+++ include/canvas2D/Path.hpp
@@ -1,5 +1,7 @@
 #ifndef PATH_HPP
 #define PATH_HPP
+#include <vector>
+#include <glm/vec2.hpp>
 
 namespace canvas2D {
 
@@ -17,10 +19,7 @@ namespace canvas2D {
 
     class Path {
     public:
-        Path();
-
-        void beginPath();
-        void closePath();
+        Path() = default;
 
         void moveTo(float x, float y);
         void lineTo(float x, float y);
@@ -28,11 +27,18 @@ namespace canvas2D {
         void bezierTo(float x1, float y1, float x2, float y2);
         void bezierTo(float x1, float y1, float x2, float y2, float x3, float y3);
 
+        void moveTo(const glm::vec2& p);
+        void lineTo(const glm::vec2& p);
+        void arcTo(const glm::vec2& p1, const glm::vec2& p2, float r);
+        void bezierTo(const glm::vec2& p1, const glm::vec2& p2);
+        void bezierTo(const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& p3);
+
         void rect(float x,float y,float w, float h);
         void roundRect(float x,float y,float w, float h, const std::initializer_list<float>& r);
         void arc(float x,float y,float r, float start, float end, bool ccw = false);
         void ellipse(float x, float y, float rx, float ry, float rot, float start, float end, bool ccw = false);
-
+    private:
+        std::vector<glm::vec2> m_data;
     };
 }
 
